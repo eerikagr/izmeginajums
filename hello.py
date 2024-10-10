93b06414339e71a4468fa2cca251f2dfb709ab75
eerikagr <egraudina12@edu.riga.lv>
2024-10-10 09:39:45 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,30 @@
+#print ("Sveika pasaule!")
+#print((738*273)**2)
+
+#print("Sveiks, Andri!")
+
+# pieprasu vārdu
+vards = input("Kā Tevi sauc?")
+
+#x=2+3
+#x=x+1
+#print(x)
+
+#a="12"
+#b="7"
+#print(a+b)
+
+#a=12
+#b=7
+#print(a+b)
+#print(b)
+#print("b")
+
+#sasveicinos
+
+
+vecums = input("Cik Tev gadu?")
+vec=int(vecums)+1
+print("Sveiks, "+vards)
+print("Tev ir", vecums, "gadi")
+print("Sveiks,", vecums, "gadi vecais", vards, "un nākamgad Tev būs", vec, "gadi.")
